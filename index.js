54f36fac1a630955ef7c12462287d64dded175a6
kovoliver <30378529+kovoliver@users.noreply.github.com>
2021-11-15 11:43:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const cors = require("cors");
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
 const io = require('socket.io')(server,  {
     cors: {
       origin: "*",
@@ -9,30 +8,10 @@ const io = require('socket.io')(server,  {
     }
 });
 
-
-const corsOpts = {
-    origin: '*',
-  
-    methods: [
-      'GET',
-      'POST',
-    ],
-  
-    allowedHeaders: [
-      'Content-Type',
-    ],
-};
-
+const mysql = require("mysql");
 const roomMembers = {};
-
 app.use('/', express.static('public'));
-app.use(cors(corsOpts));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 // app.get("/", ()=> {
 
@@ -49,25 +28,6 @@ io.on('connection', (socket) => {
 
         socket.join(roomId);
         socket.emit('room_joined', roomId);
-
-        // const roomClients = io.sockets.adapter.rooms[roomId] || { length: 0 }
-        // const numberOfClients = roomClients.length;
-        // console.log(io.sockets.adapter.rooms);
-
-        // These events are emitted only to the sender socket.
-        // if (numberOfClients == 0) {
-        //     console.log(`Creating room ${roomId} and emitting room_created socket event`)
-        //     socket.join(roomId)
-        //     socket.emit('room_created', roomId)
-        // } else if (numberOfClients == 1) {
-        //     console.log(`Joining room ${roomId} and emitting room_joined socket event`)
-        //     socket.join(roomId)
-        //     socket.emit('room_joined', roomId)
-        // } else {
-        // console.log(`Can't join room ${roomId}, emitting full_room socket event`)
-        //     socket.emit('full_room', roomId)
-        // }
-        //clientsNum++;
     });
 
     // These events are emitted to all the sockets connected to the same room except the sender.
